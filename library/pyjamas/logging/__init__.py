8a9be900a9a06cb3f5ef7e5e1faa8ad6f954297e
Luke Leighton <lkcl@lkcl.net>
2012-05-07 16:59:22 +0100

    add extra imports for pyjamas.logging

--- library/pyjamas/logging/__init__.py
+++ library/pyjamas/logging/__init__.py
@@ -10,7 +10,8 @@ __author__ = 'Peter Bittner <peter.bittner@gmx.net>'
 from pyjamas.logging.handlers import \
     AlertHandler, AppendHandler, ConsoleHandler, NullHandler
 # blatantly copy everything from CPython's logging
-from logging import getLogger, Formatter, StreamHandler, DEBUG, BASIC_FORMAT
+from logging import getLogger, Formatter, StreamHandler
+from logging import DEBUG, WARNING, INFO, BASIC_FORMAT
 
 # a handy replacement for BASIC_FORMAT printing nothing but the plain text
 PLAIN_FORMAT = '%(message)s'
